cfb515c399ce51ac9831619cce20678ce63a0151
VyasAvinash <53515377+VyasAvinash@users.noreply.github.com>
2019-09-30 15:20:01 +0200

    Update b.groovy

--- b.groovy
+++ b.groovy
@@ -1,5 +1,5 @@
 // checking for file type
-bat "echo ${env.DEBUG_COVERAGE}.toLowerCase()
+bat "echo "${env.DEBUG_COVERAGE}.toLowerCase()""
 bat """echo "thisisgroovyfile""""
 //successful
 bat "echo Avinash"
